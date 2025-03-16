06b2cca7cddaac01b9b75f55ce3ca0b72d1088e7
sfell77 <github@sfproductions.net>
2025-03-16 18:31:51 -0400

    mv SG to interface

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ resource "aws_launch_template" "im_mr_meeseeks_look_at_me" {
     }
     network_interfaces {
         associate_public_ip_address = true
+        security_groups             = [ aws_security_group.ec2_sg.id ]
     }
-    vpc_security_group_ids          = [ aws_security_group.ec2_sg.id ]
     tag_specifications {
         resource_type               = "instance"
         tags = {
